12a76300e6bebcfa6cfac24f33aa3bcc7382170f
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-04-08 13:16:42 +0200

    Improvements

---
+++ prefactored-songs.ts
@@ -0,0 +1,22 @@
+
+
+//% weight=2 color=#1174EE icon="\uf001" block="Songs"
+//% parts="songs"
+namespace songs {
+
+    //% weight=210
+    //% blockId=songs_dadadado block="Da Da Da Do"
+    //% icon="\uf001"
+    //% blockExternalInputs=1
+    //% parts="songs"
+    export function dadadado(): void {
+        music.playTone(392, music.beat(BeatFraction.Half))
+        music.playTone(392, music.beat(BeatFraction.Half))
+        music.playTone(392, music.beat(BeatFraction.Half))
+        music.playTone(311, music.beat(BeatFraction.Whole))
+        music.playTone(349, music.beat(BeatFraction.Half))
+        music.playTone(349, music.beat(BeatFraction.Half))
+        music.playTone(349, music.beat(BeatFraction.Half))
+        music.playTone(294, music.beat(BeatFraction.Whole))
+    }
+}
